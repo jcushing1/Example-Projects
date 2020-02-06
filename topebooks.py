9670fc6d7c8451200014e659871b6a5614c9d97b
jcushing1 <36546405+jcushing1@users.noreply.github.com>
2020-02-06 16:06:02 -0500

    Add files via upload

---
+++ topebooks.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  1 12:47:25 2019
+
+@author: Johannah
+"""
+
+###top 50 ebooks
+
+Pride and Prejudice
+Frankenstein
+Et dukkehjem
+The Importance of Being Earnest: A Trivial Comedy for Serious People
+A Tale of Two Cities
+The Strange Case of Dr. Jekyll and Mr. Hyde
+Alice's Adventures in Wonderland
+Dracula
+The Adventures of Sherlock Holmes
+The Awakening
+Moby Dick
+A Modest Proposal
+The Picture of Dorian Gray
+Beowulf: An Anglo-Saxon Epic Poem
+Metamorphosis by Franz Kafka
+Heart of Darkness
+The Adventures of Tom Sawyer
+The Yellow Wallpaper
+The Souls of Black Folk
+War and Peace
+Memories of the Future
+Ulysses
+Great Expectations
+The Iliad
+The Margenes
+USDA Miscellaneous Publication No. 579: Building with Logs
+Jane Eyre: An Autobiography
+Dubliners
+The Romance of Lust: A classic Victorian erotic novel
+Tractatus Logico-Philosophicus
+The Journal of the American-Irish Historical Society
+Emma
+The Complete Works of William Shakespeare
+The Street of Precious Pearls
+The Count of Monte Cristo
+Prestuplenie i nakazanie. 
+Les Misérables 
+Grimms' Fairy Tales 
+A Christmas Carol in Prose; Being a Ghost Story of Christmas 
+The History of Human Marriage 
+A Study in Scarlet 
+Treasure Island 
+The Brothers Karamazov
+Il Principe
+The Complete Works of William Shakespeare
+The Street of Precious Pearls
+Wuthering Heights
+Walden, and On The Duty Of Civil Disobedience
+Pygmalion
+The Patriot
